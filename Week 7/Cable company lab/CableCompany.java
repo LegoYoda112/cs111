73934c16965b24ca80d2b8167539c358f8abfdfa
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-03-08 08:54:10 -0800

    Put full code in

--- Week 7/Cable company lab/CableCompany.java
+++ Week 7/Cable company lab/CableCompany.java
@@ -1,41 +1,62 @@
 import java.util.Scanner;
+ 
+ 
+ public class CableCompany
+ 	{
+ 		public static void main(String[] args)
+ 		{
+ 			Scanner kb = new Scanner(System.in);
+ 			
+ 			double processing_fee, service_fee, premium_channels_price, total;
+ 			int account_number, numberOf_premium_channels, numberOf_basic_connectoins;
+ 			
+ 			System.out.println("Welcome to the Cable Company Billing program!");
+ 			System.out.println("Please enter your account number: ");
+ 			account_number = kb.nextInt();
+ 			
+ 			System.out.println("Are you a residential customer or business customer?\npress R for residential, or B for business");
+ 			char customer_type = kb.next().charAt(0);
+ 			
+ 			if ((customer_type == 'R') || (customer_type == 'r'))
+ 				{
+ 					processing_fee = 4.50;
+ 					service_fee = 20.50;
+ 					
+ 					System.out.println("Please enter how many premium channels you'd like to order: ");
+ 					numberOf_premium_channels = kb.nextInt();
+ 					premium_channels_price = 7.50*numberOf_premium_channels;
+ 					total = premium_channels_price + processing_fee + service_fee;
+ 					
+ 					System.out.printf("Your total bill for today is: $%.2f\n", total);
+ 				}
+ 			
+ 				else
+ 				
+ 				
+ 				processing_fee = 15;
+				service_fee = 75;
+				premium_channels_price = 50;
+			
+				System.out.println("Please enter the number of basic connections");
+				numberOf_basic_connectoins = kb.nextInt();
+				
+				System.out.println("Please enter the number of premium channels");
+				numberOf_premium_channels = kb.nextInt();
+			
+					if(numberOf_basic_connectoins<=10)
+						{
+					total = processing_fee + service_fee + premium_channels_price*numberOf_premium_channels;
+						}
+						
+						else{
+							total = processing_fee + service_fee + 5*(numberOf_basic_connectoins-10) + premium_channels_price*numberOf_premium_channels;
+								}
 
-public class CableCompany{
-	
-	public static void main(String args[]){
-		int accountNumber, numberOf_basic_connectoins, numberOf_premium_channels;
-		double processing_fee, service_fee, premium_channels_price, total = 0;
-		char customerType;
-		Scanner kb = new Scanner(System.in);
+
+ 				System.out.printf("Your bill total is $%.2f", total);
 		
-		System.out.println("Please enter your account number");
-		accountNumber = kb.nextInt();
-		
-		System.out.println("Please enter your customer type r or b");
-		customerType = kb.next().charAt(0);
-		
-		System.out.println(accountNumber + " " + customerType);
-		
-		if(customerType == 'r'|| customerType == 'R'){
-			
-			
-		}else{
-			processing_fee = 15;
-			service_fee = 75;
-			premium_channels_price = 50;
-			
-			System.out.println("Please enter the number of basic connections");
-			numberOf_basic_connectoins = kb.nextInt();
-			System.out.println("Please enter the number of premium channels");
-			numberOf_premium_channels = kb.nextInt();
-			
-			if(numberOf_basic_connectoins<=10){
-				total = processing_fee + service_fee + premium_channels_price*numberOf_premium_channels;
-			}else{
-				total = processing_fee + service_fee + 5*(numberOf_basic_connectoins-10) + premium_channels_price*numberOf_premium_channels;
-			}
-		}
-		System.out.printf("Your bill total is $%.2f", total);
-	}
-		
-}
+ 		}
+ 	}
+ 
+ 
+ 
