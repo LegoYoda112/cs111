8f76121bb4cfae5c4e745c3cf2d31dd081a1c4db
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-03-15 08:36:28 -0700

    Commenting!

--- Week 9/src/DateThirdTryDemo.java
+++ Week 9/src/DateThirdTryDemo.java
@@ -9,11 +9,18 @@
 * Date: 3/15/2018
 
 ---------------------------------------------------------------------------------------------------------------*/
+
+//Defines the DateThirdTryDemo class
 public class DateThirdTryDemo {
+	//Defines the main method
 	public static void main(String[] args){
+		//Instantiates DateThirdTry
 		DateThirdTry date = new DateThirdTry();
+		//Declares and initializes year
 		int year = 1882;
+		//Set the date in date using the setDateMethod
 		date.setDate(6, 17, year);
+		//Writes the date out by calling the writeOutput method
 		date.writeOutput();
 	}
 	
--- Week 9/src/DemoOfDateSecondTry.java
+++ Week 9/src/DemoOfDateSecondTry.java
@@ -9,12 +9,18 @@
 * Date: 3/15/2018
 
 ---------------------------------------------------------------------------------------------------------------*/
+//Defines the DemoOfDateSecondTry class
 public class DemoOfDateSecondTry {
+	//Defines the main method
 	public static void main(String[] args){
+		//Instantiates DateSecondTry
 		DateSecondTry date = new DateSecondTry();
+		//Calls the readInput method
 		date.readInput();
 		
+		//Sets day number equal to day using the getDay method
 		int dayNumber = date.getDay();
+		//Prints out the number of the month that the day is
 		System.out.println("That is the " + dayNumber + "th day of the month");
 	}
 }
