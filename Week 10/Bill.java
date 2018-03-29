6162977c0e62ba9e96f3a94e5dc30c19786cd621
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-03-29 08:33:01 -0700

    Added the Bill comments

--- Week 10/Bill.java
+++ Week 10/Bill.java
@@ -1,13 +1,19 @@
+//Imports the scanner class
 import java.util.Scanner;
 
+//Defines the Bill class
 public class Bill{
+	//Sets a rate constant equal to 150
 	public static final double RATE = 150.00;
 	
+	//Declares private hours, minutes and fee variables
 	private int hours;
 	private int minutes;
 	private double fee;
 	
+	//Defines the inputTimeWorked method
 	public void inputTimeWorked(){
+		//Prompts the user for the hours and minutes worked
 		System.out.println("Enter the number of full hours worked");
 		System.out.println("followed by the number of minutes:");
 		Scanner keyboard = new Scanner(System.in);
@@ -16,16 +22,21 @@ public class Bill{
 	}
 	
 	private double computeFee(int hoursWorked, int minutesWorked){
+		//Computes the minutes worked
 		minutesWorked = hoursWorked*60+minutesWorked;
+		//Computes the quarter hours worked
 		int quarterHours = minutesWorked/15;
+		//Returns the fee
 		return quarterHours*RATE;
 	}
 	
 	public void updateFee(){
+		//Calculates the fee based off of hours and minutes
 		fee = computeFee(hours, minutes);
 	}
 	
 	public void outputBill(){
+		//Rrints out the hours/minutes worked, the rate and the total fee
 		System.out.println("Time worked: ");
 		System.out.println(hours + " hours and " + minutes + " minutes");
 		System.out.println("Rate: $"+RATE + " per quarter hour.");
--- Week 10/BillingDialog.java
+++ Week 10/BillingDialog.java
@@ -1,12 +1,18 @@
+//Defines the BillingDialog class
 public class BillingDialog
 {
+	//Main method
 	public static void main(String[] args){
+		//Displays a welcome message
 		System.out.println("Welcome to the law offices of");
 		System.out.println("Dewey, Cheatham, and Howe.");
+		//Instantiates a new Bill object
 		Bill yourBill = new Bill();
+		//Calls the input time worked, update fee and output bill methods
 		yourBill.inputTimeWorked();
 		yourBill.updateFee();
 		yourBill.outputBill();
+		//Displayes a thank you message
 		System.out.println("We have placed a lien on your house.");
 		System.out.println("It has been our pleasure to work with you.");
 	}
