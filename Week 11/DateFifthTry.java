5d2e4a7093dd8024c8e7db7cb395bc34df7aa149
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-04-03 08:41:54 -0700

    Added date fifth try

--- Week 11/DateFifthTry.java
+++ Week 11/DateFifthTry.java
@@ -1,24 +1,81 @@
 /*------------------------------------------------------------------------------------------------------------
 
-* Program Name: DateThirdTry.java
+* Program Name: DateFifthTry.java
 
 * Programmer Name: Thomas Godden
 
 * Description: A class that contains date reading, writing and interpreting methods
 
-* Date: 3/29/2018
+* Date: 4/3/2018
 
 ---------------------------------------------------------------------------------------------------------------*/
 
 //imports scanner
 import java.util.Scanner;
 
-public class DateFourthTry {
+public class DateFifthTry {
 	//Declares variables
 	private String month;
 	private int day;
 	private int year;
 	
+	public void writeOutput(){
+		System.out.println(month + " " + day + ", " + year);
+	}
+	
+	public void readInput(){
+		boolean tryAgain = true;
+		Scanner keyboard = new Scanner(System.in);
+		while (tryAgain){
+			System.out.println("Enter month, day, and year");
+			System.out.println("as three integers:");
+			System.out.println("do not use commas or other punctuations.");
+			int monthInput = keyboard.nextInt();
+			int dayInput = keyboard.nextInt();
+			int yearInput = keyboard.nextInt();
+			if(dateOK(monthInput, dayInput, yearInput)){
+				setDate(monthInput, dayInput,yearInput);
+				tryAgain = false;
+			}else
+				System.out.println("Illegal date. Reenter input.");
+		}
+	}
+	
+	public void setDate(int month, int date, int year){
+		if (dateOK(month, date, year)){
+			this.month = monthString(month);
+			this.day = day;
+			this.year = year;
+		}else{
+			System.out.println("Fatal Error");
+			System.exit(0);
+		}
+	}
+	
+	public void setMonth(int monthNumber){
+		if((monthNumber <=0)|| (monthNumber > 12)){
+			System.out.println("Fatal Error");
+			System.exit(0);
+		}else{
+			month = monthString(monthNumber);
+		}
+	}
+	
+	public void setDay(int day){
+		if((day<=0) || (day>31)){
+			System.out.println("Fatal Error");
+			System.exit(0);
+		}else
+			this.day = day;
+	}
+	
+	public void setYear(int year){
+		if((year<=1000) || (year>9999)){
+			System.out.println("Fatal Error");
+			System.exit(0);
+		}else
+			this.year = year;
+	}
 	
 	//Defines the toString method
 	public String toString()
@@ -28,24 +85,16 @@ public class DateFourthTry {
 	}
 	
 	//Defines the equals class
-	public boolean equals(DateFourthTry otherDate){
+	public boolean equals(DateFifthTry otherDate){
 		//Return a boolean value depending on if the date equals otherDate
-		return((month.equals(otherDate.month)) && (day == otherDate.day) && (year == otherDate.year));
+		return((month.equalsIgnoreCase(otherDate.month)) && (day == otherDate.day) && (year == otherDate.year));
 	}
 	
-	public boolean precedes(DateFourthTry otherDate){
+	public boolean precedes(DateFifthTry otherDate){
 		//Return a boolean value depending on if the date is before otherDate
 		return( (year<otherDate.year) || (year == otherDate.year && getMonth() < otherDate.getMonth()) || (year == otherDate.year && month.equals(otherDate.month) && day<otherDate.day) );
 	}
 	
-	//Defines the setDate method, which has parameters: newMonth, newDay and newYear
-	public void setDate (int newMonth, int newDay, int newYear){
-		//Sets month equal to the String equivalent of newMonth using the monthString method
-		month = monthString(newMonth);
-		//Sets day and year equal to the newDay and newYear parameters
-		day = newDay;
-		year = newYear;
-	}
 	
 	//Defines the monthString method, with parameter monthNumber
 	public String monthString(int monthNumber){
@@ -83,24 +132,8 @@ public class DateFourthTry {
 		}
 	}
 	
-	//Defines the writeOutput method
-	public void writeOutput(){
-		//Prints the month day and year
-		System.out.println(month + " " + day + ", " + year);
-	}
-	
-	//Defines the readInput method
-	public void readInput()
-	{
-		//Declares and initializes the keyboard Scanner
-		Scanner keyboard = new Scanner(System.in);
-		//Prompts the user for the month day and year
-		System.out.println("Enter month, day and year.");
-		System.out.println("Do not use a comma.");
-		//Scans for the next inputed String, int and int
-		month = keyboard.next();
-		day = keyboard.nextInt();
-		year = keyboard.nextInt();
+	private boolean dateOK(int monthInt, int dayInt, int yearInt){
+		return (((monthInt >=1)&&(monthInt<=12)) && ((dayInt >=1)&&(monthInt<=31)) && ((yearInt >=1000)&&(yearInt<=9999)));
 	}
 	
 	//Defines the getDay method
