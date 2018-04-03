7c69ecb4d29fcf6422440933e89701f5538ca29d
LegoYoda112 <thomas.godden@outlook.com>
2018-04-02 18:40:13 -0700

    Added doc box for event

--- Week 11/Event.java
+++ Week 11/Event.java
@@ -1,16 +1,31 @@
+/*------------------------------------------------------------------------------------------------------------
+
+* Program Name: Event.java
+
+* Programmer Name: Thomas Godden
+
+* Description: Creating an event class to store and manipulate events
+
+* Date: 4/02/2018
+
+---------------------------------------------------------------------------------------------------------------*/
 public class Event{
+	//Declares the eventType and eventMinRate varialbes
 	private char eventType;
-	private double eventMinRate
+	private double eventMinRate;
 	
-	public Event(char event, double rate){
+	//Creates a method called Event that can be passed event and rate peramaters and assignes those too eventType and eventMinRate
+	public void Event(char event, double rate){
 		eventType = event;
 		eventMinRate = rate;
 	}
 	
+	//Returns eventType
 	public char getEventType(){
 		return eventType;
 	}
 	
+	//Returns eventMinRate
 	public double getEventMinRate(){
 		return eventMinRate;
 	}
