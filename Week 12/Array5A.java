601369f39afa1a90599f0fcbdda4495f934faee3
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-04-12 08:32:44 -0700

    Added Array5A

---
+++ Week 12/Array5A.java
@@ -0,0 +1,19 @@
+public class Array5A{
+	public static void main(String[] args){
+		//Creates a salary array with four elements
+		double[] salary = new double[4];
+		
+		//Assigns values to the array
+		salary[0] = 5.25;
+		salary[1] = 6.55;
+		salary[2] = 10.25;
+		salary[3] = 16.85;
+		
+		//Prints out the values in the array one by one
+		System.out.println("Salaries one by one are");
+		System.out.println(salary[0]);
+		System.out.println(salary[1]);
+		System.out.println(salary[2]);
+		System.out.println(salary[3]);
+	}
+}
