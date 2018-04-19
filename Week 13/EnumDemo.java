3952fc11098b7969484b060e02d940e008687cc1
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-04-19 08:04:25 -0700

    Added enum demo

---
+++ Week 13/EnumDemo.java
@@ -0,0 +1,10 @@
+public class EnumDemo{
+	enum WorkDay {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY};
+	
+	public static void main(String[] args){
+		WorkDay startDay = WorkDay.MONDAY;
+		WorkDay endDay = WorkDay.FRIDAY;
+		System.out.println("Work starts on " + startDay);
+		System.out.println("Work ends no " + endDay);
+	}
+}
