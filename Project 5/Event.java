a9ddd0ddfca1d92a8a2c43f81146d35b055a45f7
4804W13 <4804W13@usa.miracosta.cc.ca.us>
2018-04-24 08:15:47 -0700

    Added event 5c

---
+++ Project 5/Event.java
@@ -0,0 +1,32 @@
+/*------------------------------------------------------------------------------------------------------------
+
+* Program Name: Event.java
+
+* Programmer Name: Thomas Godden
+
+* Description: Creating an event class to store and manipulate events
+
+* Date: 4/02/2018
+
+---------------------------------------------------------------------------------------------------------------*/
+public class Event{
+	//Declares the eventType and eventMinRate varialbes
+	private char eventType;
+	private double eventMinRate;
+	
+	//Creates a method called Event that can be passed event and rate peramaters and assignes those too eventType and eventMinRate
+	public Event(char event, double rate){
+		eventType = event;
+		eventMinRate = rate;
+	}
+	
+	//Returns eventType
+	public char getEventType(){
+		return eventType;
+	}
+	
+	//Returns eventMinRate
+	public double getEventMinRate(){
+		return eventMinRate;
+	}
+}
---
+++ Project 5/Event5C.java
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------------------------------------------
+
+* Program Name: Event5C.java
+
+* Programmer Name: Thomas Godden
+
+* Description: Creates an array of events and prints out its contents
+
+* Date: 4/24/2018
+
+---------------------------------------------------------------------------------------------------------------*/
+public class Event5C{
+	public static void main(String args[]){
+		//Array of events
+		Event[] someEvents = new Event[5];
+		char x = 'c';
+		
+		//Loops through someEvents and populates the array
+		for(int i = 0; i<5; i++){
+			someEvents[i] = new Event(x, 0.0);
+		}
+		
+		//Loops through someEvents and prints out the event type and min rate for each element
+		for(int i = 0; i<5; i++){
+			System.out.print(someEvents[i].getEventType()+" ");
+			System.out.println(someEvents[i].getEventMinRate());
+			
+		}
+	}
+}
